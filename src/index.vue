cc3bf699151d9c76aedc3a643e5bdde05147ab37
dword <info@sebastianlandwehr.com>
2021-09-03 20:38:52 +0000

    fix: make value required

--- src/index.vue
+++ src/index.vue
@@ -14,7 +14,7 @@ export default {
   },
   name: 'VueMermaidComponent',
   props: {
-    value: { type: String },
+    value: { required: true, type: String },
   },
   render() {
     return <div>{this.value}</div>
