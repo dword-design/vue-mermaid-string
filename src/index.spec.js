1b5b72b381d321fd5bfb81a65442616f5f557ecd
Sebastian Landwehr <info@sebastianlandwehr.com>
2024-11-14 20:48:18 +0000

    chore: activate hasFindPort

--- src/index.spec.js
+++ src/index.spec.js
@@ -276,7 +276,7 @@ export default tester(
     },
   },
   [
-    testerPluginComponent({ componentPath: resolver.resolve('./index.vue') }),
+    testerPluginComponent({ componentPath: resolver.resolve('./index.vue'), hasFindPort: true }),
     {
       async after() {
         await this.browser.close();
