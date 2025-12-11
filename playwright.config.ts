f4fd53ee18bb2f8e506d430ac98d5d9f1d7ce936
Sebastian Landwehr <info@sebastianlandwehr.com>
2025-12-11 22:32:49 +0100

    fix: foo

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from '@playwright/test';
+import { defineConfig, devices } from '@playwright/test';
 
 export default defineConfig({
   expect: {
@@ -19,6 +19,11 @@ export default defineConfig({
     '{snapshotDir}/{testFileDir}/{testFileName}-snapshots/{arg}{-projectName}{ext}',
 
   use: {
+    ...devices['Desktop Chrome'],
+
+    // Set device scale factor to 1 for consistent rendering
+    deviceScaleFactor: 1,
+
     // Ensure consistent font rendering
     launchOptions: {
       args: [
@@ -26,7 +31,11 @@ export default defineConfig({
         '--disable-skia-runtime-opts',
         '--disable-font-subpixel-positioning',
         '--disable-lcd-text',
+        '--force-device-scale-factor=1',
       ],
     },
+
+    // Force specific viewport to ensure consistent layout
+    viewport: { height: 720, width: 1280 },
   },
 });
